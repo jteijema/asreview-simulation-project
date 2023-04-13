2ac3c38b6e6d34b69596a4193e9571541643fc00
JT <j.j.teijema@uu.nl>
2023-04-13 17:32:58 +0200

    Add metrics generation

--- docker/run_simulation.sh
+++ docker/run_simulation.sh
@@ -12,6 +12,8 @@ echo "SIMULATION_ID: $SIMULATION_ID"
 
 # Run the simulation
 asreview simulate ./synergy/$DATASET.csv -s $SIMULATION $SETTINGS
+asreview metrics $SIMULATION -o $SIMULATION.json
 
 # Run the Python script
 python upload_simulation_to_storage.py "$SIMULATION" "$SIMULATION_ID" "$BUCKET"
+python upload_simulation_to_storage.py "$SIMULATION.json" "$SIMULATION_ID" "$BUCKET"
